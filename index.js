d0f72343d298cb4343e1ad766d77890bacd48788
praneethattada <120724868+praneethattada@users.noreply.github.com>
2024-01-06 00:09:22 +0530

    added files

---
+++ index.js
@@ -0,0 +1,61 @@
+var level=0;
+var gameseq=[];
+var userseq=[];
+$("body").on("keypress",function(){
+   if(level==0)
+   gamelevel();
+
+}
+   
+    
+);
+$(".btn").click(function() {
+    userseq.push($(this).attr("id"));
+    check(userseq.length-1);
+});
+function check(current)
+{
+if(gameseq[current]==userseq[current]){
+    if(gameseq.length==userseq.length){
+    setTimeout(function () {
+        gamelevel();
+      }, 500);
+    }
+}
+
+else{
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    start();
+
+}
+}
+function gamelevel(){
+    userseq=[];
+    level++;
+    $("#level-title").text("Level " + level);
+    var num=Math.floor(Math.random()*4)+1;
+    gameseq.push(num);
+    playSound(num)
+    $("."+num).addClass("pressed");
+    setTimeout(function (){
+        $("."+num).removeClass("pressed");
+    },100);
+
+}
+function start()
+    {
+        level =0;
+        gameseq=[]
+
+    }
+    function playSound(file){
+        var audio= new Audio(file + ".mp3");
+        audio.play();
+
+    }
